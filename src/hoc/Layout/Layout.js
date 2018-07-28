0d2851585a29706577fa6a1656a653327b75fff1
Sava <sd.sakhro@gmail.com>
2018-07-28 16:01:42 +0300

    hoc: Layout

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {createMuiTheme, CssBaseline, MuiThemeProvider} from '@material-ui/core';
+import Header from "../../components/Header/Header";
+
+const theme = createMuiTheme({
+    overrides: {
+        MuiButton: {
+            root: {
+                borderRadius: 25,
+            },
+        },
+        MuiPaper: {
+            root: {
+                transform: 'translateZ(0)',
+                willChange: 'transform'
+            },
+        }
+    },
+    palette: {
+        primary: {
+            main: '#263238'
+        },
+        secondary: {
+            main: '#2196F3'
+        }
+    },
+});
+
+class Layout extends Component {
+    render() {
+        const {children} = this.props;
+
+        return (
+            <MuiThemeProvider theme={theme}>
+                <CssBaseline/>
+                <Header/>
+                {children}
+            </MuiThemeProvider>
+        )
+    }
+}
+
+export default Layout;
