b30e080c2ecef9bc77dbf33942e6dad4682486eb
Sava <sd.sakhro@gmail.com>
2018-07-29 16:25:06 +0300

    update Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
-import {AppBar, Toolbar, Typography} from '@material-ui/core';
+import {Link} from 'react-router-dom';
+import {AppBar, Toolbar} from '@material-ui/core';
+import Logo from '../../assets/imgs/logo.svg';
 
 const header = () => (
     <AppBar position="sticky">
-        <Toolbar variant="dense">
-            <Typography variant="subheading" color="inherit">
-                Portfolio
-            </Typography>
+        <Toolbar variant="dense" style={{display: 'flex', justifyContent: 'center'}}>
+            <Link to='/' style={{height: 'inherit', paddingTop: 4}}>
+                <img src={Logo} alt='Character S' style={{height: 40}}/>
+            </Link>
         </Toolbar>
     </AppBar>
 );
