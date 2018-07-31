c979480b5ea62fc2898e34799c0a11d4a4afc9d3
Sava <sd.sakhro@gmail.com>
2018-07-31 19:00:20 +0300

    Add GitHub link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,46 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {AppBar, Toolbar, withStyles} from '@material-ui/core';
+import {AppBar, Toolbar, withStyles, Button} from '@material-ui/core';
 import ChevronLeft from '@material-ui/icons/ChevronLeft'
 import Logo from '../../assets/imgs/logo.svg';
+import Github from '../../assets/imgs/github.svg';
 
 const styles = theme => ({
     backButton: {
-        cursor: 'pointer',
-        display: 'flex',
-        alignItems: 'center',
-        transition: 'color 100ms ease-in-out',
-        '&:hover': {
-            color: theme.palette.secondary.main
-        }
+        paddingRight: 20,
+        color: theme.palette.primary.contrastText
+    },
+    Link: {
+        height: 'inherit',
+        paddingTop: 4
     }
 });
 
 const header = ({classes, location, onBackClick}) => (
     <AppBar position="sticky">
         <Toolbar variant="dense"
+                 component='nav'
                  style={{display: 'flex', justifyContent: 'center', maxWidth: 1140, width: '100%', margin: 'auto'}}>
-            {location.pathname === '/'
-                ? null
-                : <span aria-label='Go back' className={classes.backButton} tabIndex={0} onClick={onBackClick}><ChevronLeft/>Back</span>}
+            <div style={{width: 90}}>
+                {location.pathname === '/'
+                    ? null
+                    : <Button aria-label='Return to previous page'
+                              size="small"
+                              className={classes.backButton}
+                              onClick={onBackClick}
+                    >
+                        <ChevronLeft/>Back
+                    </Button>}
+            </div>
             <div style={{flexGrow: 1, textAlign: 'center'}}>
-                <Link to='/'
-                      aria-label='Go to the home page'
-                      style={{
-                          height: 'inherit',
-                          paddingTop: 4,
-                          marginLeft: location.pathname === '/'
-                              ? null
-                              : -56.88
-                      }}>
-                    <img src={Logo} alt='Character S' style={{height: 40}}/>
+                <Link to='/' aria-label='Go to the home page' className={classes.Link}>
+                    <img aria-hidden="true" src={Logo} alt='Character S' style={{height: 40}}/>
                 </Link>
+            </div>
+            <div style={{width: 90, textAlign: 'right'}}>
+                <a href='https://github.com/Sakhro' target='_blank' aria-label="GitHub page" className={classes.Link}>
+                    <img aria-hidden="true" src={Github} alt='Github icon' style={{height: 25}}/>
+                </a>
             </div>
         </Toolbar>
     </AppBar>
