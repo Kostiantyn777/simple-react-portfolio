80fde1b94798502c29504ca8bc995cccae2946b5
Sava <sd.sakhro@gmail.com>
2018-08-03 20:33:38 +0300

    Change section tag to the article tag in ProjectList

--- src/components/ProjectsList/ProjectsList.js
+++ src/components/ProjectsList/ProjectsList.js
@@ -5,7 +5,7 @@ import ProjectCard from './ProjectCard/ProjectCard';
 const projectsList = ({projects, pushToProjectPage}) => (
     <Grid container spacing={16}>
         {projects.map(project => (
-            <Grid item xs={12} sm={6} md={4} key={project.id} component='section'>
+            <Grid item xs={12} sm={6} md={4} key={project.id} component='article'>
                 <ProjectCard {...project} pushToProjectPage={pushToProjectPage}/>
             </Grid>
         ))}
